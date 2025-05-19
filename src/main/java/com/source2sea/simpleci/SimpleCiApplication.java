38eae8ad0aba5a7496fdd511fdf22a01f1fd7ab7
Steffen F. Qvistgaard <stqv@source2sea.com>
2025-05-19 09:18:28 +0200

    executable file

---
+++ src/main/java/com/source2sea/simpleci/SimpleCiApplication.java
@@ -0,0 +1,13 @@
+package com.source2sea.simpleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleCiApplication.class, args);
+  }
+
+}
